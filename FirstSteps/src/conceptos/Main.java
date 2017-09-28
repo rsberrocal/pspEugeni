406218901be63b573d3dd5beb586e5808cc709af
Richard Sudario Berrocal <rsberrocal@gmail.com>
2017-09-28 19:52:45 +0200

    Acabado el coso de jefes

--- FirstSteps/src/conceptos/Main.java
+++ FirstSteps/src/conceptos/Main.java
@@ -4,18 +4,33 @@ import java.util.Random;
 
 public class Main {
 
+    public static BigBoss levelUp(Empleado emp,double incentivo){
+        BigBoss nextBoss = new BigBoss(emp.getNombre(),emp.getSueldo(),emp.getSeccion(),incentivo);
+        return nextBoss;
+    }
+
     public static void main(String[] args) {
         Empleado[] emp = new Empleado[3];
+        BigBoss finalBoss = new BigBoss();
         Random rnd = new Random();
+        String nombre;
         for(int j=0;j<emp.length;j++){
-            emp[j]=new Empleado(("Empleado constructor1 "+j), rnd.nextInt(100)*(j+1));
-        }
-        for(int i=0;i<emp.length;i++){
-            if(emp[i].getNombre().contains("2")){
-                emp[i].addSueldo(150);
+            nombre = "Empleado "+j;
+            if(j==0){
+                nombre = "Richard";
             }
-            System.out.println("Nombre:"+emp[i].getNombre());
-            System.out.println("Sueldo:"+emp[i].getSueldo());
+            emp[j]=new Empleado(nombre, 120);
+
+        }
+        finalBoss=levelUp(emp[0],1000);
+        for(int i=0;i<emp.length;i++){
+            if(emp[i].getNombre()==finalBoss.getNombre()){
+                System.out.println("Nombre del jefe: "+finalBoss.getNombre());
+                System.out.println("Sueldo: "+finalBoss.getSueldo());
+            }else{
+                System.out.println("Nombre:"+emp[i].getNombre());
+                System.out.println("Sueldo:"+emp[i].getSueldo());
+            }
         }
     }
 }

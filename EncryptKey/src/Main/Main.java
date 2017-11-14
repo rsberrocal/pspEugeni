9d57dc907200f574c2d4d69a1d14c34bf14bca23
Richard Sudario Berrocal <rsberrocal@gmail.com>
2017-11-14 17:29:46 +0100

    Empezado la primera activad a entregar

---
+++ EncryptKey/src/Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dame un numero del 1 al 99");
+
+    }
+}

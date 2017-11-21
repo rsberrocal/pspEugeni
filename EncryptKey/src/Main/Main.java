b111e5be429ed3505b567123fa8c112165c1107e
Richard Sudario Berrocal <rsberrocal@gmail.com>
2017-11-21 20:05:34 +0100

    Ya desincripta y esta todo comentado

--- EncryptKey/src/Main/Main.java
+++ EncryptKey/src/Main/Main.java
@@ -8,98 +8,129 @@ import javax.xml.bind.DatatypeConverter;
 import java.lang.reflect.Array;
 import java.security.MessageDigest;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.Scanner;
 
 public class Main {
-
+    //Funcion para encriptar con el hash
     public static String encryptWithHash(SecretKey superKey,String userText){
         byte[]textByte=null;
         try {
-
+            //Se crea la instancia
             Cipher cyberman = Cipher.getInstance("AES");
+            //Se asigna el modo encrypt junto con la llave
             cyberman.init(Cipher.ENCRYPT_MODE,superKey);
+            //Se recogen en un array los bytes del texto que ha puesto el usuario
             textByte=userText.getBytes();
+            //Y se encripta finalmente
             textByte=cyberman.doFinal(textByte);
         }catch (Exception e){
+            //Si sale algun error se muestra y au
             e.printStackTrace();
         }
-        return DatatypeConverter.printHexBinary(textByte);
+        //Se devuelve el texto cifrado en un idioma entendible
+        return DatatypeConverter.printBase64Binary(textByte);
     }
-
+    //Funcion para generar un hash mediante un numero como contraseña
     public static SecretKey genHash(int numPass){
         SecretKey FinalKey=null;
         String pass;
-
+        //Si el numero es menor de 10 se le añade un 0 para tener dos bytes
         if(numPass<10){
             pass="0"+String.valueOf(numPass);
         }else{
             pass=String.valueOf(numPass);
         }
         try{
+            //Se genera la instancia del hash
             MessageDigest md = MessageDigest.getInstance("SHA-256");
+            //Se crea el hash con el numero como contraseña
             byte[] hash=md.digest(pass.getBytes("UTF-8"));
             byte[] key= Arrays.copyOf(hash,16);
+            //Se crea una llave simetrica
             FinalKey= new SecretKeySpec(key,"AES");
         }catch (Exception e){
+            //Por si sale algun error
             e.printStackTrace();
         }
+        //Se devuelve la llave
         return FinalKey;
     }
 
+    //Funcion para desencriptar
     public static byte[] decryptWithHash(SecretKey superKey,String encryptedText){
         byte[]textByte=null;
         try {
-
-            Cipher Cyberman = Cipher.getInstance("AES");
+            //Se crea la instancia
+            Cipher Cyberman = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            //Se le asigna el modo y la llave
             Cyberman.init(Cipher.DECRYPT_MODE,superKey);
-            textByte=Cyberman.doFinal(encryptedText.getBytes());
+            //Se desencripta el texto y se pasa en un array de bytes con Base64
+            textByte=Cyberman.doFinal(DatatypeConverter.parseBase64Binary(encryptedText));
         }catch (Exception e){
-            e.printStackTrace();
-            return null;
+            //El error que suele salir es que la llave no es la correcta y textByte queda en null
+            return textByte;
         }
+        //Se devuelve el array de bytes
         return textByte;
     }
-
+    //Funcion para encontrar la llave que desencripta el texto
     public static void discoverKey(String encryptedText){
+        //Boolean para saber cuando se ha encontrado
         boolean found=false;
         int i=0;
         SecretKey posibleKey=null;
         byte[] posibleText;
-        while(!found){
+        System.out.println("Buscando el texto descifrado");
+        //Bucle para encontrar cada posiblidad de llave
+        while(!found||i>=100){
+            //Se genera un hash con el numero que toca
             posibleKey=genHash(i);
+            //Se intenta desincriptar con esa llave
             posibleText=decryptWithHash(posibleKey,encryptedText);
-            System.out.println(posibleKey);
+            //Si no es un null es que la ha encontrado
             if(posibleText!=null){
-
+                System.out.println("¡¡Encontrado!!");
+                //Se pasa el array de bits a Base64 para desencriptarlo y que salga el texto original
+                System.out.println(new String(Base64.getDecoder().decode(DatatypeConverter.printBase64Binary(posibleText))));
+                //Al encontrarse la frase se pone el boolean a true
                 found=true;
             }else{
+                //Si no la encuentra suma el contador para la siguiente iteracion
                 i++;
             }
         }
     }
 
     public static void main(String [] args){
+        //Variables principales
         SecretKey mainKey;
         int numPass;
         String textEncrypted;
         Scanner sc = new Scanner(System.in);
-
-
+        //Bucle para controlar que el numero este en el rango aceptable
         do{
+            //Se pide el numero al usuario
             System.out.println("Dame un numero del 1 al 99");
             numPass=sc.nextInt();
+            //Para eleminar el buffer
             sc.nextLine();
+            //Si no entra en el rango se le avisa al usuario
             if(numPass>=100||numPass<=0){
                 System.out.println("Numero incorrecto");
             }else{
+                //Si entra en el rango se crea primero la llave
                 mainKey=genHash(numPass);
+                //Se pide que el usuario una frase para cifrar
                 System.out.println("Dame una frase a cifrar");
                 textEncrypted=sc.nextLine();
+                //Se encripta la frase
                 textEncrypted=encryptWithHash(mainKey,textEncrypted);
+                //Y se muestra al usuario
+                System.out.println("Texto encriptado");
                 System.out.println(textEncrypted);
-                System.out.println(DatatypeConverter.parseHexBinary(textEncrypted).length);
-                System.out.println(textEncrypted.getBytes().length);
-                //discoverKey(textEncrypted);
+                //Por ultimo se busca que frase era sin pillar la variable
+                discoverKey(textEncrypted);
             }
         }while(numPass>=100||numPass<=0);
 
